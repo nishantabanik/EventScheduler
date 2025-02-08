2e8e200b78de201f13e7bc3808c78f5438153cd7
Razvan Andrei <chiper_raizy@yahoo.com>
2025-02-08 11:31:14 +0100

    adding the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
+import logo from "../images/banana.png";
+
 const Header = () => {
+  function handleClick() {
+    console.log("clicked");
+  }
+
   return (
-    <>
-      <p className="text-6xl text-[#000814]">Header</p>
-    </>
+    <header className="flex bg-[#000814] w-full h-[7rem]">
+      <a className="w-22 md:w-38 hover:scale-105 transition-all ">
+        <img
+          src={logo}
+          alt="Logo"
+          className="bg-[#ffd60a]  h-[7rem] ml-[7rem]"
+        />
+      </a>
+
+      <div className=" w-full flex justify-end items-center mr-10">
+        <button
+          className="bg-[#ffd60a] text-[#000814] text-2xl px-12 rounded-xl hover:text-3xl"
+          onClick={handleClick}
+        >
+          Sign in
+        </button>
+      </div>
+    </header>
   );
 };
 

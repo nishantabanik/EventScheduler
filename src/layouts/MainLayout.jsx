8c35a67f53c370b8caf37053ed36901af4cda438
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-02-09 15:50:24 +0100

    added header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div className="bg-[#FFD60A] text-gray-300 flex flex-col min-h-screen">
+      <Navbar />
       <Header />
       <main className=" py-4">
         <Outlet />

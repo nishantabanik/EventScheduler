8cde0d9ecb044d9a2c9a5a7ae043c96189754245
Nishanta Banik <nishanta.banik2016@gmail.com>
2025-02-09 13:41:59 +0100

    Adding the Homepage & EventDetails changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import NotFound from "./pages/NotFound";
 import Contact from "./pages/Contact";
+import EventDetails from "./pages/EventDetails"; // Import EventDetails
+
 function App() {
   return (
-    <>
+    <Router> {/* Wrapping everything with Router */}
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
@@ -15,10 +17,11 @@ function App() {
           <Route path="signin" element={<SignIn />} />
           <Route path="signout" element={<SignOut />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="events/:id" element={<EventDetails />} /> {/* Event Details Route */}
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 

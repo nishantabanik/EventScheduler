11ca6215e2b64bc0915a6c0e0e81473588aa157c
Razvan Andrei <chiper_raizy@yahoo.com>
2025-02-12 06:34:14 +0100

    adding colorPallete and Font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
           </a>
           <div className=" flex flex-row justify-evenly xl:justify-center ">
             <div className="xl:flex text-[#ffd60a] xl:flex-col text-sm xl:justify-center xl:items-start  xl:ml-[14rem] xl:w-[22rem]">
-              <h5>Berlin</h5>
-              <h5>Weiskopffstraße 16</h5>
-              <h5>12459 Berlin</h5>
-              <h5>berlin@banana.de</h5>
+              <h5 className="text-accent font-serif">Berlin</h5>
+              <h5 className="font-serif">Weiskopffstraße 16</h5>
+              <h5 className="font-serif">12459 Berlin</h5>
+              <h5 className="font-serif">berlin@banana.de</h5>
             </div>
 
             <div className="xl:flex text-[#ffd60a] xl:flex-col text-sm xl:justify-center xl:items-start   xl:w-[22rem]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#001D3D", // main color
+        secondary: "#000814", // secondary color
+        tertiary: "#003566", //tertiary color
+        accent: "#ffd60a", // accent color
+      },
+      fontFamily: {
+        serif: ["Ubuntu", "sans-serif"], // set Ubuntu as the default font
+      },
+    },
   },
   plugins: [daisyui],
 };
